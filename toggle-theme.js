a0c9dc1032ed58cdaf8c457a3baff9bc84dab4d3
mgv <mglasner10@gmail.com>
2022-09-25 23:22:35 -0300

    footer

--- toggle-theme.js
+++ toggle-theme.js
@@ -3,11 +3,16 @@ const button = document.getElementById("toggle-theme-button");
 button.addEventListener("click", () => {
 
   let navbar = document.getElementById("myNavbar");
+  let footer = document.getElementById("myFooter");
 
   navbar.classList.toggle("bg-light");
   navbar.classList.toggle("bg-dark");
   navbar.classList.toggle("navbar-light");
   navbar.classList.toggle("navbar-dark");
+  footer.classList.toggle("bg-light");
+  footer.classList.toggle("bg-dark");
+  footer.classList.toggle("navbar-light");
+  footer.classList.toggle("navbar-dark");
 
   if (navbar.className.includes("navbar-light")) {
     document.body.style.backgroundColor = "#F8F9FA";
